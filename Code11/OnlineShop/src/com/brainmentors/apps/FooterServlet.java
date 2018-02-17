e079be5a811709a4530b43f512ae20e1095cb78c
amit4alljava <amit4alljava@gmail.com>
2018-02-17 17:30:29 +0530

    Codes

---
+++ Code11/OnlineShop/src/com/brainmentors/apps/FooterServlet.java
@@ -0,0 +1,34 @@
+package com.brainmentors.apps;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FooterServlet
+ */
+@WebServlet("/footer")
+public class FooterServlet extends HttpServlet {
+	
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.println("<p>Terms and Conditions</p>");
+		out.println("</body></html>");
+		out.close();
+	}
+
+}
---
+++ Code11/OnlineShop/src/com/brainmentors/apps/HeaderServlet.java
@@ -0,0 +1,36 @@
+package com.brainmentors.apps;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HeaderServlet
+ */
+@WebServlet("/header")
+public class HeaderServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		String color = (String)request.getAttribute("mycolor");
+		out.println("<html><head>");
+		//out.println("<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' integrity='sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u' crossorigin='anonymous'>");
+		out.println("</head><body bgcolor='"+color+"'>");
+		//out.close();
+	}
+
+}
+
+
